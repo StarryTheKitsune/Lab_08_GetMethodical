dcf58846c07b6f3367585bc2d6b1d6ad706a8b6f
Robyn Specht <starryfox7@gmail.com>
2024-11-02 16:38:58 -0400

    final

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -201,47 +201,4 @@ public class SafeInput
         }while(!done);
         return retVal;
     }
-
-    /**
-     *
-     * @param msg a message designated by the coder to display to the user in the fancy border
-     */
-    public static void prettyHeader(String msg){
-        int length = msg.length();
-        int spacer = (52 - length) / 2;
-        int count = 1;
-
-        while(count <= 60){
-            System.out.print("*");
-            count++;
-        }
-        System.out.println();
-        while(count >= 61 && count <= 63){
-            System.out.print("*");
-            count++;
-        }
-        while(count < 63 + spacer && count >= 64){
-            System.out.print(" ");
-            count++;
-        }
-        while(count == 63 + spacer){
-            System.out.print(msg);
-            count+=length;
-        }
-        while(count >= 63 + spacer + length && count <= 117){
-            System.out.print(" ");
-            count++;
-        }
-        while (count >= 118 && count <= 120){
-            System.out.print("*");
-            count++;
-        }
-        System.out.println();
-        while (count >= 121 && count <= 180){
-            System.out.print("*");
-            count++;
-        }
-
-
-    }
 }
